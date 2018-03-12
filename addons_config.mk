3c30056cc069416480a5f4c1fa8f01538260b189
Christopher Baker <me@christopherbaker.net>
2018-03-11 22:33:32 -0500

    Update addons_config.make.

--- addons_config.mk
+++ addons_config.mk
@@ -1,8 +1,9 @@
-ADDON_NAME = ofxPlayer
+meta:
+	ADDON_NAME = ofxPlayer
 	ADDON_DESCRIPTION = A collection of tools for playing back any data sequence, including images, sensors, paths, etc.
 	ADDON_AUTHOR = bakercp
 	ADDON_TAGS = "data" "playback" "video" "buffer" "utilities"
 	ADDON_URL = http://github.com/bakercp/ofxPlayer
 
 common:
-	ADDON_DEPENDENCIES = ofxPlayer ofxIO
+	ADDON_DEPENDENCIES = ofxIO
